4bbc53e211ca6087deb6fa7f9127206a2096fcde
yhattori <yushi.t.hattori@gmail.com>
2025-04-07 05:09:24 +0000

    Updated dashboard

--- programs/nixCats/lua/nvim/plugins/snacks.lua
+++ programs/nixCats/lua/nvim/plugins/snacks.lua
@@ -99,7 +99,37 @@ end, { desc = "Open :H with help completion" })
 require('snacks').setup({
   animate = { enabled = true },
   bigfile = { enabled = true },
-  dashboard = { enabled = true },
+  dashboard = {
+    enabled = true,
+    sections = {
+      { section = "header" },
+      {
+        pane = 2,
+        section = "terminal",
+        cmd = "",
+        height = 5,
+        padding = 1,
+      },
+      { section = "keys", gap = 1, padding = 1 },
+      { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+      { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+      {
+        pane = 2,
+        icon = " ",
+        title = "Git Status",
+        section = "terminal",
+        enabled = function()
+          return Snacks.git.get_root() ~= nil
+        end,
+        cmd = "git status --short --branch --renames",
+        height = 5,
+        padding = 1,
+        ttl = 5 * 60,
+        indent = 3,
+      },
+      { section = "startup" },
+    },
+  },
   debug = { enabled = true },
   -- dim = { enabled = true },
   -- explorer = { enabled = true },
